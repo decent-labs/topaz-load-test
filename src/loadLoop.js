d174b820847bc213b4832536715c5884c7a91a04
Adam Gall <adam@decentcrypto.com>
2019-04-30 09:40:33 -0400

    Create a file which does the main startup work of creating apps, then looping over making hashes

---
+++ src/loadLoop.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const makeApps = require('./make/apps');
+const makeHashes = require('./make/hashes');
+
+const start = () => {
+  const numAccounts = parseInt(process.env.NUMBER_OF_NEW_APPS, 10);
+  const chunkedSize = parseInt(process.env.REQUEST_CHUNK_SIZE, 10);
+  const minInterval = parseInt(process.env.MIN_APP_INTERVAL_S, 10);
+  const maxInterval = parseInt(process.env.MAX_APP_INTERVAL_S, 10);
+
+  return makeApps(numAccounts, chunkedSize, minInterval, maxInterval).then(apis => loop(apis, chunkedSize));
+};
+
+const loop = (apis, chunkSize) => {
+  return makeHashes(apis, chunkSize).then(_ => loop(apis, chunkSize));
+};
+
+module.exports = start;
