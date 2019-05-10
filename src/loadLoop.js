8191186fb5c81ea910f7f67b173f36b070af47ed
Adam Gall <adam@decentcrypto.com>
2019-05-10 16:44:00 -0400

    Rewrite to attempt to have more control over how hashes are created.
    Not quite there, but it's a start

--- src/loadLoop.js
+++ src/loadLoop.js
@@ -5,16 +5,23 @@ const makeApps = require('./make/apps');
 const makeHashes = require('./make/hashes');
 
 const start = () => {
-  const numAccounts = parseInt(process.env.NUMBER_OF_NEW_APPS, 10);
-  const chunkedSize = parseInt(process.env.REQUEST_CHUNK_SIZE, 10);
+  const numApps = parseInt(process.env.NUMBER_OF_NEW_APPS, 10);
+  const appReqChunkSize = parseInt(process.env.APP_REQ_CHUNK_SIZE, 10);
+
+  const numHashesPerApp = parseInt(process.env.NUM_HASHES_PER_APP, 10);
+  const hashPerAppChunk = parseInt(process.env.HASH_PER_APP_CHUNK, 10);
+  
   const minInterval = parseInt(process.env.MIN_APP_INTERVAL_S, 10);
   const maxInterval = parseInt(process.env.MAX_APP_INTERVAL_S, 10);
 
-  return makeApps(numAccounts, chunkedSize, minInterval, maxInterval).then(apis => loop(apis, chunkedSize));
+  return makeApps(numApps, appReqChunkSize, minInterval, maxInterval)
+    .then(apis => loop(apis, numHashesPerApp, hashPerAppChunk));
 };
 
-const loop = (apis, chunkSize) => {
-  return makeHashes(apis, chunkSize).then(_ => loop(apis, chunkSize)).catch(console.log);
+const loop = (apis, numHashesPerApp, hashPerAppChunk) => {
+  return makeHashes(apis, numHashesPerApp, hashPerAppChunk)
+    .then(_ => loop(apis, numHashesPerApp, hashPerAppChunk))
+    .catch(console.log);
 };
 
 module.exports = start;
